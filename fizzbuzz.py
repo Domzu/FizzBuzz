b5cb5a8239fe175779b5a58dc2f69106b5026dd9
Domzu <imdominicmanuel1@gmail.com>
2022-10-26 01:36:02 +0800

    Checked and Editted code quality using pylint

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,17 @@
+'''
+function for fizzbuzz
+'''
 def fizzbuzz():
-    
+
     for number in range(1,101):
-        
+
         if number%3==0 and number%5==0:
             number = 'FizzBuzz'
         elif number%3==0:
             number = 'Fizz'
         elif number%5==0:
             number = 'Buzz'
-            
+
         print(number)
 
 fizzbuzz()
